2cb64fc114b81492163bcc1fb47072e5eac930f6
clement <clementmumpa01@gmail.com>
2026-04-18 12:12:29 +0300

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'> 
+      <h2 className='section-title'>about us</h2>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo, assumenda dolorum. Non laboriosam tenetur dolor sunt corporis, a
+        ccusamus nesciunt at maiores iure magni commodi deleniti assumenda harum totam pariatur iusto quae? Quasi tenetur m
+        ollitia, quos adipisci minima, beatae perferendis porro sit dolores ea aliquid? Expedita vero quam unde. Error, totam!
+      </p>
+    </section>
+  )
+}
+
+export default About
